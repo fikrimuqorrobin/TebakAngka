8c86128c5a508f35b5150b78fae4b72193854cc3
fikrimuqorrobin <fikrimuqorrobin@gmail.com>
2017-11-29 13:42:26 +0700

    Commit pertama tebak angka

---
+++ src/id/co/indocyber/Angka.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.co.indocyber;
+
+/**
+ *
+ * @author user
+ */
+public class Angka {
+    
+    public void generateAngkaAcak(){
+        
+    }
+    
+}
